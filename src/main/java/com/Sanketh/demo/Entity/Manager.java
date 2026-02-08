b25969e8a543dcc00d71a62827fc0ef8a91dd547
sanketh908 <sankeths908@gmail.com>
2026-02-08 14:42:17 +0530

    added something

--- src/main/java/com/Sanketh/demo/Entity/Manager.java
+++ src/main/java/com/Sanketh/demo/Entity/Manager.java
@@ -6,11 +6,17 @@ import java.util.List;
 @Entity
 @Table(name = "manager_table")
 public class Manager {
+    @Column(name = "manager_id",nullable = false,unique = true)
     private long id;
+    @Column(name = "manager_name",nullable = false)
     private String name;
+    @Column(name = "manager_email",nullable = false,unique = true)
     private String email;
+    @Column(name = "manager_password",nullable = false)
     private String password;
+    @Column(name = "manager_department",nullable = false)
     private String department;
+    @Column(name = "manager_contact",nullable = false,unique = true)
     private String contact;
 
     @OneToMany(mappedBy = "Manager",cascade = CascadeType.ALL)
