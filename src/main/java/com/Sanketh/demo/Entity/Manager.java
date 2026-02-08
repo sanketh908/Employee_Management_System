826398efedcb0ce49e01d478809c2b0b8662801d
sanketh908 <sankeths908@gmail.com>
2026-02-08 13:24:18 +0530

    added something

--- src/main/java/com/Sanketh/demo/Entity/Manager.java
+++ src/main/java/com/Sanketh/demo/Entity/Manager.java
@@ -1,4 +1,20 @@
 package com.Sanketh.demo.Entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
+@Table(name = "manager_table")
 public class Manager {
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String department;
+    private String contact;
+
+    @OneToMany(mappedBy = "Manager",cascade = CascadeType.ALL)
+    private List<Employee> employeeList;
+    @OneToMany(mappedBy = "Manager",cascade = CascadeType.ALL)
+    private List<Duty> dutyList;
 }
