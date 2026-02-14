35898e6c64ca677f61cc1f0a94eea09bc0145764
Sanketh <sankeths908@gmail.com>
2026-02-14 14:59:30 +0530

    Delete src directory

--- src/main/java/com/Sanketh/demo/Repository/DutyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Sanketh.demo.Repository;
-
-import com.Sanketh.demo.Entity.Duty;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface DutyRepository extends MongoRepository<Duty, ObjectId> {
-}
--- src/main/java/com/Sanketh/demo/Repository/LeaveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Sanketh.demo.Repository;
-
-import com.Sanketh.demo.Entity.Leave;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface LeaveRepository extends MongoRepository<Leave, ObjectId> {
-}
