b8dabd9c987444d2ed67fef472c1793d62307d08
sanketh908 <sankeths908@gmail.com>
2026-02-16 17:50:05 +0530

    string password generating method

--- src/main/java/com/Sanketh/EmployeeManagementSystem/Service/AdminService.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/Service/AdminService.java
@@ -46,4 +46,22 @@ public class AdminService {
         Random random = new Random();
         return random.nextInt(1000,9999);
     }
+    public int geneateRandomPassword(int lenght)
+    {
+      String upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      String lower="abcdefghijklmnopqrstuvwxyz";
+      String number="0123456789";
+      String spchar="!@#$%^&*()_+";
+      String combin=upper+lower+number+spchar;
+      StringBuilder stringBuilder=new StringBuilder();
+      Random random=new Random();
+      stringBuilder.append(upper.charAt(random.nextInt(upper.length())));
+      stringBuilder.append(lower.charAt(random.nextInt(lower.length())));
+      stringBuilder.append(number.charAt(random.nextInt(number.length())));
+      stringBuilder.append(spchar.charAt(random.nextInt(spchar.length())));
+      for(int i=4;i<lenght;i++)
+      {
+          stringBuilder.append(combin.charAt(random.nextInt(combin.length())));
+      }
+    }
 }
