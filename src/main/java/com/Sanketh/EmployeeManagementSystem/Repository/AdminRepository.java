ebe2ac0ad59dc6ddd3618698c66545969e0acb91
sanketh908 <sankeths908@gmail.com>
2026-02-16 16:54:05 +0530

    Email setup

--- src/main/java/com/Sanketh/EmployeeManagementSystem/Repository/AdminRepository.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/Repository/AdminRepository.java
@@ -4,7 +4,9 @@ import com.Sanketh.EmployeeManagementSystem.Entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
-
+ public Optional<Admin> findAdminByUsernameAndPassword(String username,String password);
 }
--- src/main/java/com/Sanketh/EmployeeManagementSystem/Service/AdminService.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/Service/AdminService.java
@@ -1,11 +1,30 @@
 package com.Sanketh.EmployeeManagementSystem.Service;
 
 import com.Sanketh.EmployeeManagementSystem.Entity.*;
+import com.Sanketh.EmployeeManagementSystem.Repository.AdminRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface AdminService {
-    public Admin checkAdminlogin(String username,String password);
+public class AdminService {
+    private final AdminRepository adminRepository;
+
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    public Admin checkAdminlogin(String username,String password)
+    {
+        Optional<Admin> admin= adminRepository.findAdminByUsernameAndPassword(username,password);
+        if(admin.isPresent())
+        {
+            return admin.get();
+        }
+        else
+        {
+            return null;
+        }
+    }
     public Manager addManager(Manager manager);
     public List<Manager> getAllManagers();
     public String deleteManager();
