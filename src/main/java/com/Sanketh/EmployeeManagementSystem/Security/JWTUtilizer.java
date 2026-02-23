0fca8dbba956f4dd6f0420c96e1059780689ca55
sanketh908 <sankeths908@gmail.com>
2026-02-23 20:03:26 +0530

    Added JWTUtilizer JWT token generate

--- src/main/java/com/Sanketh/EmployeeManagementSystem/Security/JWTUtilizer.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/Security/JWTUtilizer.java
@@ -1,11 +1,13 @@
 package com.Sanketh.EmployeeManagementSystem.Security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,11 +15,17 @@ import java.util.Map;
 public class JWTUtilizer {
         private static final String SECRET_KEY_STRING="am9pbmFzaWRldGVsbGhlYXJpbmdkcm9wcGVkcG93ZXJmdWxleGFtaW5lY2F1Z2h0a2U=";
         private final SecretKey secretKey= Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes());
-        public String generateJWTTokens(String username, String password){
+        public String generateJWTTokens(String username, String role){
             Map<String,Object> claims=new HashMap<>();
             claims.put("username",username);
-            claims.put("password",password);
-            return Jwts.builder().claim();
+            claims.put("role",role);
+            return Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(username)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000 *60*60*2))
+                    .signWith(secretKey, SignatureAlgorithm.HS256).compact();
         }
+
 }
 
