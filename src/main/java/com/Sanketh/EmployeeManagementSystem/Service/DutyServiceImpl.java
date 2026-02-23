66677a8aec990c1fb57845cb84a4214cec21bc63
sanketh908 <sankeths908@gmail.com>
2026-02-23 18:05:48 +0530

    Added duty service impl

--- src/main/java/com/Sanketh/EmployeeManagementSystem/Service/DutyServiceImpl.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/Service/DutyServiceImpl.java
@@ -1,17 +1,51 @@
 package com.Sanketh.EmployeeManagementSystem.Service;
 
+import com.Sanketh.EmployeeManagementSystem.Entity.Admin;
 import com.Sanketh.EmployeeManagementSystem.Entity.Duty;
+import com.Sanketh.EmployeeManagementSystem.Entity.Employee;
+import com.Sanketh.EmployeeManagementSystem.Entity.Manager;
+import com.Sanketh.EmployeeManagementSystem.Repository.AdminRepository;
+import com.Sanketh.EmployeeManagementSystem.Repository.DutyRepository;
+import com.Sanketh.EmployeeManagementSystem.Repository.EmployeeRepository;
+import com.Sanketh.EmployeeManagementSystem.Repository.ManagerRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class DutyServiceImpl implements  DutyService {
+    private final  DutyRepository dutyRepository;
+    private final EmployeeRepository employeeRepository;
+    private final AdminRepository adminRepository;
+    private final ManagerRepository managerRepository;
+
+    public DutyServiceImpl(DutyRepository dutyRepository, EmployeeRepository employeeRepository, AdminRepository adminRepository, ManagerRepository managerRepository) {
+        this.dutyRepository = dutyRepository;
+        this.employeeRepository = employeeRepository;
+        this.adminRepository = adminRepository;
+        this.managerRepository = managerRepository;
+    }
+
     @Override
     public Duty assignDutyByAdminToEmployee(Duty duty, Integer employeeId, Integer adminId) {
+        Optional<Employee> employee = employeeRepository.findById(employeeId);
+        Optional<Admin> admin = adminRepository.findById(adminId);
+        if (employee.isPresent() && admin.isPresent()) {
+            duty.setEmployee(employee.get());
+            duty.setAssiendByAdmin(admin.get());
+            return dutyRepository.save(duty);
+        }
         return null;
     }
 
     @Override
     public Duty assignDutyByAdminToManager(Duty duty, Integer managerId, Integer adminId) {
+        Optional<Manager> manager = managerRepository.findById(managerId);
+        Optional<Admin> admin = adminRepository.findById(adminId);
+        if (manager.isPresent() && admin.isPresent()) {
+            duty.setManager(manager.get());
+            duty.setAssiendByAdmin(admin.get());
+            return dutyRepository.save(duty);
+        }
         return null;
     }
 
