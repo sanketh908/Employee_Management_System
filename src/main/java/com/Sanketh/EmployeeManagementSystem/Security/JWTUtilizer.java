ee76c293cf7114ed11280985c2db57f9f8ee2fdf
sanketh908 <sankeths908@gmail.com>
2026-02-24 07:31:36 +0530

    new way to create a JWT

--- src/main/java/com/Sanketh/EmployeeManagementSystem/Security/JWTUtilizer.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/Security/JWTUtilizer.java
@@ -6,15 +6,27 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class JWTUtilizer {
-        private static final String SECRET_KEY_STRING="am9pbmFzaWRldGVsbGhlYXJpbmdkcm9wcGVkcG93ZXJmdWxleGFtaW5lY2F1Z2h0a2U=";
-        private final SecretKey secretKey= Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes());
+    String secretKey="";
+        JWTUtilizer() throws NoSuchAlgorithmException {
+            KeyGenerator keyGenerator=KeyGenerator.getInstance("HmacSHA256");
+            SecretKey SecretKey=keyGenerator.generateKey();
+            secretKey= Base64.getEncoder().encodeToString(SecretKey.getEncoded());
+        }
+        public Key getKey()
+        {
+                return  Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+         }
         public String generateJWTTokens(String username, String role){
             Map<String,Object> claims=new HashMap<>();
             claims.put("username",username);
@@ -24,7 +36,7 @@ public class JWTUtilizer {
                     .setSubject(username)
                     .setIssuedAt(new Date())
                     .setExpiration(new Date(System.currentTimeMillis() + 1000 *60*60*2))
-                    .signWith(secretKey, SignatureAlgorithm.HS256).compact();
+                    .signWith(getKey(), SignatureAlgorithm.HS256).compact();
         }
 
 }
