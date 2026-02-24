f81baeea5bc0a682fe0519d795fc1c611872f221
sanketh908 <sankeths908@gmail.com>
2026-02-24 18:09:50 +0530

    Added LoginRequest class

--- src/main/java/com/Sanketh/EmployeeManagementSystem/DataTransferringObject/LoginRequest.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/DataTransferringObject/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.Sanketh.EmployeeManagementSystem.DataTransferringObject;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    private String identifier; // Can be username or email
+    private String password;
+
+
+
+
 }
