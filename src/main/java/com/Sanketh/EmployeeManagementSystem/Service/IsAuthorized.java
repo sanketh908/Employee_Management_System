4131a800857ad460ec1a903ea8df012c464f2586
sanketh908 <sankeths908@gmail.com>
2026-02-26 17:06:34 +0530

    Added TestIsAuthorized

--- src/main/java/com/Sanketh/EmployeeManagementSystem/Service/IsAuthorized.java
+++ src/main/java/com/Sanketh/EmployeeManagementSystem/Service/IsAuthorized.java
@@ -1,4 +1,21 @@
 package com.Sanketh.EmployeeManagementSystem.Service;
 
+import com.Sanketh.EmployeeManagementSystem.Security.JWTUtilizer;
+import org.springframework.stereotype.Service;
+
+@Service
 public class IsAuthorized {
+    private final JWTUtilizer jwtUtilizer;
+
+    public IsAuthorized(JWTUtilizer jwtUtilizer) {
+        this.jwtUtilizer = jwtUtilizer;
+    }
+    public boolean isAuthorized(String header,String requiredRole) {
+        String token = header.substring(7);
+        if(jwtUtilizer.validateToken(token).get("role").equalsIgnoreCase(requiredRole)) {
+            return true;
+        }
+        return false;
+
+    }
 }
